67a7491b0aa301b6581df61208abb6fea010c09d
sollarp <szpitor@gmail.com>
2023-02-06 20:24:38 +0000

    test commit

--- app/src/main/java/com/example/call_mapbox_api/util/Extensions.kt
+++ app/src/main/java/com/example/call_mapbox_api/util/Extensions.kt
@@ -19,6 +19,20 @@ fun showKeyboard(view: View, context: Context) {
         getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.showSoftInput(view , InputMethodManager.SHOW_FORCED)
 }
+/*
+fun onSearchQuery(letter: String,addresses: List<EvPointsEntity>):  {
+    val listAddresses = addresses.map { it.AddressInfo }
+        .filter { it.AddressLine1?.split(" ")?.any { word -> word.startsWith(letter, ignoreCase = true) } == true ||
+                it.AddressLine2?.split(" ")?.any { word -> word.startsWith(letter, ignoreCase = true) } == true ||
+                it.Postcode?.startsWith(letter, ignoreCase = true) == true ||
+                it.Title?.startsWith(letter, ignoreCase = true) == true ||
+                it.Town?.startsWith(letter, ignoreCase = true) == true }
+}
+*/
+
+/*fun onSearchQuery(letter: String, addresses: (EvPointsEntity) -> ItemDataConverter): List<EvPointsEntity> {
+    return addresses.filter { it.split(" ").any { word -> word.startsWith(letter, ignoreCase = true) } }
+}*/
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
