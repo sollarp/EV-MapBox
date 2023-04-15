fa815cbcf5f3a4a0e4f1fc936d600b1346026e2a
Peter Szollar <74240451+sollarp@users.noreply.github.com>
2023-04-15 11:19:14 +0100

    Delete app/build directory
    Manually deleted build folder

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_ev_mapbox_MyApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.ev_mapbox.MyApplication"
-)
-public final class _com_example_ev_mapbox_MyApplication {
-}
